f90a46e6205048cbd15a5c2236d1d6a90c6bd5a3
Bogdan Matuszyczk <bogdan.utn@gmail.com>
2019-04-08 18:09:33 -0300

    packjson

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
-    historyApiFallback: true
+    historyApiFallback: true,
+    https: true,
   },
   externals: {
     // global app config object
